e57bc9fac3bf6e86ee5be20a9dd434882f9e5c5a
mfields <468369+mfields@users.noreply.github.com>
2020-08-17 08:12:38 -0700

    test(ie): add IE 9 and 10 to windows test script

--- karma/win.js
+++ karma/win.js
@@ -2,7 +2,17 @@ const { sharedConfig } = require('./shared.js')
 
 module.exports = function (config) {
   const c = sharedConfig()
-  c.browsers = ['Chrome', 'Firefox', 'Safari', 'Opera', 'IE']
+  c.browsers = ['Chrome', 'Firefox', 'Opera', 'IE', 'IE10', 'IE9']
   c.logLevel = config.LOG_INFO
+  c.customLaunchers = {
+    IE10: {
+      base: 'IE',
+      'x-ua-compatible': 'IE=EmulateIE10'
+    },
+    IE9: {
+      base: 'IE',
+      'x-ua-compatible': 'IE=EmulateIE9'
+    }
+  }
   config.set(c)
 }
